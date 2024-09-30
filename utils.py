226a907e774bd74162f7bca2a21c95c7b513ee14
palkolex <67223838+palkolex@users.noreply.github.com>
2024-09-30 14:03:10 +0200

    vla les trucs utiles (sans jeu de mot 😉)

--- utils.py
+++ utils.py
@@ -1,7 +1,20 @@
+from data import check_char_number_validity
+
+def is_a_valid_number(number):
+    i = 0
+    is_a_valid_char = True 
+    while is_a_valid_char == True and i <= len (number):
+        is_a_valid_char = check_char_number_validity (number [i])
+        i = i + 1
+    return is_a_valid_char
+
 def ask_for_the_init_number():
-    input ("Entrez votre nombre: ")
-    init_number = input
-    return 
+    init_number = input ("Entrez votre nombre: ")
+    while not (is_a_valid_number):
+        init_number= input("Nombre incorrect; réessayez: ")
+    return init_number
+
+
 
 
 
