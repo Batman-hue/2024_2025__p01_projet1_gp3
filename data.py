fd6b314019c565a1b54df27390d5f5113890bf60
Damiann Broudou <broudoudamiann@gmail.com>
2024-09-30 14:09:29 +0200

    et voila

--- data.py
+++ data.py
@@ -1,22 +1,36 @@
+ask_for_the_init_base_text = "La base ?"
+ask_again_for_the_init_base_text = "LA BASE "
+
+
+def ask_for_the_init_base ():
+    init_base = input(ask_for_the_init_base_text)
+    while not (is_a_valid_base (init_base)) == True:
+        init_base = input (ask_again_for_the_init_base_text)
+    return init_base
 
 
 
-# def ask_for_the_init_base (init_base):
-#     init_base = input("Enter the init base: ")
-#     base = 2, 10, 16
-#     return init_base
+def is_a_valid_base(base):
+    i = 0
+    is_a_valid_base = True
+    while is_a_valid_base == True and i <= len (base) - 1:
+        is_a_valid_base = check_base_base_validity (base[i])
+        i = i + 1
+    return is_a_valid_base
 
-# print (ask_for_the_init_base())
+def check_base_base_validity(basen):
+    return basen in hex_base_valid_base
 
+bin_number_valid_chars = ["0", "1"]
 
-# def is_a_valid_base(base):
-#     i = 0
-#     is_a_valid_base = True
-#     while is_a_valid_base == True and i <= len (base) - 1:
-#         is_a_valid_base = 
+dec_number_valid_chars = \
+    bin_number_valid_chars \
+  + ["2", "3", "4", "5", "6", "7", "8", "9"]
 
+hex_number_valid_chars = \
+    dec_number_valid_chars \
+  + ["A", "B", "C", "D", "E", "F"] \
+  + ["a", "b", "c", "d", "e", "f"]
 
-def hufeu_dhjhjdhf_djj():
-    dfuuf = int('320', 2)
-
-print(hufeu_dhjhjdhf_djj)
+def check_char_number_validity (char):
+    return char in hex_number_valid_chars
