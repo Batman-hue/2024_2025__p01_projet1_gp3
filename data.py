76441844d1ab171582554c7b37d56d05a5c0fcd2
palkolex <67223838+palkolex@users.noreply.github.com>
2024-09-30 15:22:00 +0200

    je suis trop fort

--- data.py
+++ data.py
@@ -1,36 +1,36 @@
-ask_for_the_init_base_text = "La base ?"
-ask_again_for_the_init_base_text = "LA BASE "
+# ask_for_the_init_base_text = "La base ?"
+# ask_again_for_the_init_base_text = "LA BASE "
 
 
-def ask_for_the_init_base ():
-    init_base = input(ask_for_the_init_base_text)
-    while not (is_a_valid_base (init_base)) == True:
-        init_base = input (ask_again_for_the_init_base_text)
-    return init_base
+# def ask_for_the_init_base ():
+#     init_base = input(ask_for_the_init_base_text)
+#     while not (is_a_valid_base (init_base)) == True:
+#         init_base = input (ask_again_for_the_init_base_text)
+#     return init_base
 
 
 
-def is_a_valid_base(base):
-    i = 0
-    is_a_valid_base = True
-    while is_a_valid_base == True and i <= len (base) - 1:
-        is_a_valid_base = check_base_base_validity (base[i])
-        i = i + 1
-    return is_a_valid_base
+# def is_a_valid_base(base):
+#     i = 0
+#     is_a_valid_base = True
+#     while is_a_valid_base == True and i <= len (base) - 1:
+#         is_a_valid_base = check_base_base_validity (base[i])
+#         i = i + 1
+#     return is_a_valid_base
 
-def check_base_base_validity(basen):
-    return basen in hex_number_valid_chars
+# def check_base_base_validity(basen):
+#     return basen in hex_number_valid_chars
 
-bin_number_valid_chars = ["0", "1"]
+# bin_number_valid_chars = ["0", "1"]
 
-dec_number_valid_chars = \
-    bin_number_valid_chars \
-  + ["2", "3", "4", "5", "6", "7", "8", "9"]
+# dec_number_valid_chars = \
+#     bin_number_valid_chars \
+#   + ["2", "3", "4", "5", "6", "7", "8", "9"]
 
-hex_number_valid_chars = \
-    dec_number_valid_chars \
-  + ["A", "B", "C", "D", "E", "F"] \
-  + ["a", "b", "c", "d", "e", "f"]
+# hex_number_valid_chars = \
+#     dec_number_valid_chars \
+#   + ["A", "B", "C", "D", "E", "F"] \
+#   + ["a", "b", "c", "d", "e", "f"]
 
-def check_char_number_validity (char):
-    return char in hex_number_valid_chars
+# def check_char_number_validity (char):
+#     return char in hex_number_valid_chars
--- utils.py
+++ utils.py
@@ -1,20 +1,49 @@
-from data import check_char_number_validity
+# from data import*
 
-def is_a_valid_number(number):
-    i = 0
-    is_a_valid_char = True 
-    while is_a_valid_char == True and i <= len (number):
-        is_a_valid_char = check_char_number_validity (number [i])
-        i = i + 1
-    return is_a_valid_char
+# ask_for_the_init_number_text = "Nombre choisi:"
 
-def ask_for_the_init_number():
-    init_number = input ("Entrez votre nombre: ")
-    while not (is_a_valid_number):
-        init_number= input("Nombre incorrect; réessayez: ")
-    return init_number
+# ask_again_for_the_init_number_text = "Nombre incorrect; réessaie : "
 
+# def check_char_number_validity (char):
+#     return char in hex_number_valid_chars
 
+# def is_a_valid_number (number):
+#     i = 0
+#     is_a_valid_char = True
+#     while is_a_valid_char == True and i <= len (number) - 1:
+#         is_a_valid_char = check_char_number_validity (number [i])
+#         i = i + 1
+#     return is_a_valid_char
+
+# def ask_for_the_init_number ():
+#     init_number = input (ask_for_the_init_number_text)
+#     while not (is_a_valid_number (init_number)) == True:
+#         init_number = input (ask_again_for_the_init_number_text)
+#     return init_number
+        
+# ask_for_the_init_number ()
+
+ask_for_the_target_base_text = "Choissisez une base entre binaire, héxadécimal ou décimal: "
+
+ask_again_for_the_target_base_text = "Base incorrect; réessaie : "
+
+def ask_for_the_target_base ():
+    answer_target_base = input (ask_for_the_target_base_text)
+    while True:
+        if answer_target_base == "binaire":
+           target_base = 2
+           break
+        elif answer_target_base == "décimal":
+           target_base = 10
+           break
+        elif answer_target_base == "héxadécimal":
+            target_base = 16
+            break
+        else:
+            answer_target_base = input(ask_again_for_the_target_base_text)
+    return target_base
+
+ask_for_the_target_base()
 
 
 
