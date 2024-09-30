beee89a2d1934ca09014b9954f1a44a4ca738fc8
Damiann Broudou <broudoudamiann@gmail.com>
2024-09-30 14:01:57 +0200

    yejjjy

--- data.py
+++ data.py
@@ -1,8 +1,22 @@
 
-def ask_for_the_init_base (init_base):
-    base = 2, 10, 16
-    input ("Enter the init base: ")
-    return init_base
 
-print (ask_for_the_init_base())
 
+# def ask_for_the_init_base (init_base):
+#     init_base = input("Enter the init base: ")
+#     base = 2, 10, 16
+#     return init_base
+
+# print (ask_for_the_init_base())
+
+
+# def is_a_valid_base(base):
+#     i = 0
+#     is_a_valid_base = True
+#     while is_a_valid_base == True and i <= len (base) - 1:
+#         is_a_valid_base = 
+
+
+def hufeu_dhjhjdhf_djj():
+    dfuuf = int('320', 2)
+
+print(hufeu_dhjhjdhf_djj)
