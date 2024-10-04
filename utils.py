9d248e3d3c708d01105daa0e922ceaa72588d312
palkolex <67223838+palkolex@users.noreply.github.com>
2024-10-04 11:39:33 +0200

    DAMIAN BOUHH

--- utils.py
+++ utils.py
@@ -1,27 +1,27 @@
-# from data import*
+from data import*
 
-# ask_for_the_init_number_text = "Nombre choisi:"
+ask_for_the_init_number_text = "Nombre choisi:"
 
-# ask_again_for_the_init_number_text = "Nombre incorrect; réessaie : "
+ask_again_for_the_init_number_text = "Nombre incorrect; réessaie : "
 
-# def check_char_number_validity (char):
-#     return char in hex_number_valid_chars
+def check_char_number_validity (char):
+    return char in hex_number_valid_chars
 
-# def is_a_valid_number (number):
-#     i = 0
-#     is_a_valid_char = True
-#     while is_a_valid_char == True and i <= len (number) - 1:
-#         is_a_valid_char = check_char_number_validity (number [i])
-#         i = i + 1
-#     return is_a_valid_char
+def is_a_valid_number (number):
+    i = 0
+    is_a_valid_char = True
+    while is_a_valid_char == True and i <= len (number) - 1:
+        is_a_valid_char = check_char_number_validity (number [i])
+        i = i + 1
+    return is_a_valid_char
 
-# def ask_for_the_init_number ():
-#     init_number = input (ask_for_the_init_number_text)
-#     while not (is_a_valid_number (init_number)) == True:
-#         init_number = input (ask_again_for_the_init_number_text)
-#     return init_number
+def ask_for_the_init_number ():
+    init_number = input (ask_for_the_init_number_text)
+    while not (is_a_valid_number (init_number)) == True:
+        init_number = input (ask_again_for_the_init_number_text)
+    return init_number
         
-# ask_for_the_init_number ()
+ask_for_the_init_number ()
 
 # ask_for_the_target_base_text = "Choissisez une base entre binaire, héxadécimal ou décimal: "
 
