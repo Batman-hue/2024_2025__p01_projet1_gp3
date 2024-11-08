960d8ce06b242804c30ec4caafbce697a340686d
Damiann Broudou <broudoudamiann@gmail.com>
2024-11-08 11:37:40 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,3 @@
-bin_number_valid_chars = ["0","1"]
-dec_number_valid_chars = bin_number_valid_chars + ["2", "3", "4", "5", "6", "7", "8", "9"]
-hex_number_valid_chars = dec_number_valid_chars + ["A", "B", "C", "D", "E", "F","a", "b", "c", "d", "e", "f"]
+bin_number_valid_chars = ["0","1"]     # Définit les nombres utilisables en base 2.
+dec_number_valid_chars = bin_number_valid_chars + ["2", "3", "4", "5", "6", "7", "8", "9"]     # Définit les nombres utilisables en base 10.
+hex_number_valid_chars = dec_number_valid_chars + ["A", "B", "C", "D", "E", "F","a", "b", "c", "d", "e", "f"]     # Définit les nombres utilisables en base 16.
